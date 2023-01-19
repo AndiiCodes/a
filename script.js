6dd0784b49fa4d7b44ac3775ff4c5edb8d61befb
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-01-19 22:31:39 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// import Typed from 'typed.js';
+
+var typed = new Typed(".auto-type" ,{
+
+    strings: ['Front-End Developer &#129321;', 'Freelancer &#128578;', 'Snapchat Lens Creator &#128519;'],
+    typeSpeed: 100,
+    backSpeed: 50,
+    loop: true
+
+})
+
+
+
+// var typed = new Typed('.auto-type', options);
