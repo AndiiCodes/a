328b25544f2a67597c6db44b676a890b648b72e8
AndiiCodes <116770908+AndiiCodes@users.noreply.github.com>
2023-01-21 19:12:10 +0000

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ var typed = new Typed(".auto-type" ,{
 
 
 // var typed = new Typed('.auto-type', options);
+
+
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+const menu_items = document.querySelectorAll('nav .mainMenu li a');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+// close menu when you click on a menu item 
+menu_items.forEach(item => {
+    item.addEventListener('click',function(){
+        close();
+    })
+})
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+
+
+
+
